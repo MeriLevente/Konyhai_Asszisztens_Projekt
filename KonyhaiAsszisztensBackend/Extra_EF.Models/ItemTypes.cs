375c7924a68cf74abdd9f11840d6fd880240cf7a
kaplon15 <gergely.kaplon@students.jedlik.eu>
2025-03-11 12:41:18 +0100

    elmeletileg ez a backend ha valamit tonkrevagott eskuszom nemakartam xd

---
+++ KonyhaiAsszisztensBackend/Extra_EF.Models/ItemTypes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extra_EF.Models
+{
+    [Table("itemtypes")]
+    public class ItemTypes
+    {
+        [Key]
+        public required int Id { get; set; }
+        [Column("name_HU")]
+        public required string Name_HU { get; set; }
+        [Column("name_EN")]
+        public required string Name_EN { get; set; }
+        [Column("image")]
+        public required string Image { get; set; }
+    }
+}
