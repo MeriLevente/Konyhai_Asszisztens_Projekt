49deb11c1f9b54b8c3220f800becc9445153a409
kaplon15 <gergely.kaplon@students.jedlik.eu>
2025-04-01 18:45:29 +0100

    rest of the models

---
+++ Backend/KonyhaiAsszisztensBackend/Models/Items.cs
@@ -0,0 +1,41 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    [Table("items")]
+    public class Items
+    {
+        [Column("id"), Key, SwaggerIgnore]
+        public int Id { get; set; }
+
+        [Column("name"), StringLength(50), NotNull]
+        public string Name { get; set; }
+
+        [Column("name_EN"), StringLength(50), NotNull]
+        public string Name_EN { get; set; }
+
+        [Column("typeId"), NotNull]
+        public int TypeId { get; set; }
+
+        [Column("unit"), StringLength(10)]
+        public string? Unit { get; set; }
+
+        [Column("image")]
+        public string? Image { get; set; }
+
+        [JsonIgnore, SwaggerIgnore]
+        public List<Stores>? Stored { get; set; }
+
+        [JsonIgnore, SwaggerIgnore]
+        public List<Contains>? Contained { get; set; }
+    }
+}
---
+++ Backend/KonyhaiAsszisztensBackend/Models/Recipes.cs
@@ -0,0 +1,47 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    [Table("recipes")]
+    public class Recipes
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column("name"), StringLength(40), NotNull]
+        public string Name { get; set; }
+
+        [Column("name_EN"), StringLength(40), NotNull]
+        public string Name_EN { get; set; }
+
+        [Column("description"), NotNull]
+        public string Description { get; set; }
+
+        [Column("description_EN"), NotNull]
+        public string Description_EN { get; set; }
+
+        [Column("type"), StringLength(3), NotNull]
+        public string Type { get; set; }
+
+        [Column("difficulty"), NotNull]
+        public int Difficulty { get; set; }
+
+        [Column("time"), NotNull]
+        public int Time { get; set; }
+
+        [Column("image")]
+        public string? Image { get; set; }
+
+        [SwaggerIgnore]
+        public ICollection<Contains>? Ingredients { get; set; }
+    }
+}
