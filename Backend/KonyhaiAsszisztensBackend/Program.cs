f2a82a7324ed51604acec604337cc658eb3e2a76
MeriLevente <meril.levente05@gmail.com>
2025-04-03 17:02:44 +0200

    changes for docker

--- Backend/KonyhaiAsszisztensBackend/Program.cs
+++ Backend/KonyhaiAsszisztensBackend/Program.cs
@@ -8,34 +8,38 @@ using Microsoft.OpenApi.Models;
 using Swashbuckle.Swagger;
 using Microsoft.CodeAnalysis.Options;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<DataContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
-
-builder.Services.AddAuthentication("GuidAuthentication")
-        .AddScheme<AuthenticationSchemeOptions, AuthService>("GuidAuthentication", options => { });
-
-//builder.Services.AddDatabaseContext(connectionString);
-
-builder.Services.AddEndpointsApiExplorer();
-
-
-builder.Services.AddSwaggerGen(option =>
+public class Program
 {
-    option.SwaggerDoc("v1", new OpenApiInfo { Title = "DKAP Backend", Version = "v1" });
-    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    public static void Main(string[] args)
     {
-        In = ParameterLocation.Header,
-        Description = "Please enter a valid token",
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        BearerFormat = "JWT",
-        Scheme = "Bearer"
-    });
-    option.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
+
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddDbContext<DataContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+        builder.Services.AddAuthentication("GuidAuthentication")
+                .AddScheme<AuthenticationSchemeOptions, AuthService>("GuidAuthentication", options => { });
+
+
+        builder.Services.AddEndpointsApiExplorer();
+
+
+        builder.Services.AddSwaggerGen(option =>
+        {
+            option.SwaggerDoc("v1", new OpenApiInfo { Title = "DKAP Backend", Version = "v1" });
+            option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+            option.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
         {
             new OpenApiSecurityScheme
             {
@@ -47,24 +51,26 @@ builder.Services.AddSwaggerGen(option =>
             },
             new string[]{}
         }
-    });
-});
+            });
+        });
 
-var app = builder.Build();
+        var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        //app.UseCors(builder=> builder.AllowAnyHeader().AllowAnyMethod().WithOrigins(["http://localhost:5173", "http://localhost:5098"]));
+        app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+        app.UseAuthentication();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-//app.UseCors(builder=> builder.AllowAnyHeader().AllowAnyMethod().WithOrigins(["http://localhost:5173", "http://localhost:5098"]));
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
-
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
