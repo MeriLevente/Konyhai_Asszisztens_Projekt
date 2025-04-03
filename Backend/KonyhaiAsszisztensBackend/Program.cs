af9e4fa287cdff5935000fc6fd249f9cd1073a5f
MeriLevente <meril.levente05@gmail.com>
2025-04-03 17:26:19 +0200

    changes for docker 2

--- Backend/KonyhaiAsszisztensBackend/Program.cs
+++ Backend/KonyhaiAsszisztensBackend/Program.cs
@@ -8,38 +8,40 @@ using Microsoft.OpenApi.Models;
 using Swashbuckle.Swagger;
 using Microsoft.CodeAnalysis.Options;
 
-public class Program
+namespace KonyhaiAsszisztensBackend
 {
-    public static void Main(string[] args)
+    public class Program
     {
-
-        var builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddControllers();
-        string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-        builder.Services.AddDbContext<DataContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
-
-        builder.Services.AddAuthentication("GuidAuthentication")
-                .AddScheme<AuthenticationSchemeOptions, AuthService>("GuidAuthentication", options => { });
-
-
-        builder.Services.AddEndpointsApiExplorer();
-
-
-        builder.Services.AddSwaggerGen(option =>
+        public static void Main(string[] args)
         {
-            option.SwaggerDoc("v1", new OpenApiInfo { Title = "DKAP Backend", Version = "v1" });
-            option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<DataContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+            builder.Services.AddAuthentication("GuidAuthentication")
+                    .AddScheme<AuthenticationSchemeOptions, AuthService>("GuidAuthentication", options => { });
+
+
+            builder.Services.AddEndpointsApiExplorer();
+
+
+            builder.Services.AddSwaggerGen(option =>
             {
-                In = ParameterLocation.Header,
-                Description = "Please enter a valid token",
-                Name = "Authorization",
-                Type = SecuritySchemeType.Http,
-                BearerFormat = "JWT",
-                Scheme = "Bearer"
-            });
-            option.AddSecurityRequirement(new OpenApiSecurityRequirement
-            {
+                option.SwaggerDoc("v1", new OpenApiInfo { Title = "DKAP Backend", Version = "v1" });
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
         {
             new OpenApiSecurityScheme
             {
@@ -51,26 +53,27 @@ public class Program
             },
             new string[]{}
         }
+                });
             });
-        });
 
-        var app = builder.Build();
+            var app = builder.Build();
 
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            //app.UseCors(builder=> builder.AllowAnyHeader().AllowAnyMethod().WithOrigins(["http://localhost:5173", "http://localhost:5098"]));
+            app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
         }
-
-        //app.UseCors(builder=> builder.AllowAnyHeader().AllowAnyMethod().WithOrigins(["http://localhost:5173", "http://localhost:5098"]));
-        app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
-
-        app.UseAuthentication();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
     }
 }
